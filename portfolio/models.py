8e1da19dbe4149d3b1c96342e5a7ea2f86e8d6f9
Mohamed Jeafer <34100676+Mohamed-Jeafer@users.noreply.github.com>
2020-10-27 12:05:56 +0400

    linked the pages, added attributes to html pages

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,6 @@ class Portfolio(models.Model):
     description = models.CharField(max_length=255)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
