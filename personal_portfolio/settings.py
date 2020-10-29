b80b17a7852112c6fd695b2ddc22c1fa9f86864f
Mohamed Jeafer <mohd.ryad@gmail.com>
2020-10-29 13:01:32 +0000

    deployed on pythonanywhere.com

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')o2h6nxi-#(&92=#z@s0t_jh*mjhzesyn6w+l3=k++^zfz^=qk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bossmohd.pythonanywhere.com']
 
 # Application definition
 
